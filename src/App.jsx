efb76e58e0c5efca69999c3482946cf3175f5155
Ivan <racioglobus@yandex.ru>
2024-06-15 17:19:24 +0300

    edit main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import cyberLogo from '/cyber-eye.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.warnerbros.com/movies/blade-runner" target="_blank">
+          <img src={cyberLogo} className="logo" alt="Cyber logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Blade Runner Corp</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        We will organize the elimination of replicants that have gone out of control
       </p>
     </>
   )
